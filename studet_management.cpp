e2e5a5d6013724621155a9c6103480ddf8b628bb
AbenezerGemena <abenezercheb@gmail.com>
2024-11-11 00:24:23 +0300

    student register function

--- studet_management.cpp
+++ studet_management.cpp
@@ -27,3 +27,53 @@ void studentProfile(const Student& student) {
     cout << "Dormitory B" << student.block << ", R-" << student.room << "\t\t" << "Section " << student.section << endl;
 }
 
+void saveStudentToFile(const Student& student) {
+    ofstream file("student.txt", ios::app); // Open the file in append mode
+    if (file.is_open()) {
+        file << "Full Name: " << student.name << "\t\t\t" << "Program: " << student.department << endl;
+        file << "ID Number: " << student.id << "\t\t\t" << "Admission: " << student.admission << endl;
+        file << "Admission Year: " << student.admission << "/" << student.admissionYear + 1 << "\t\t" << "Class Year: " << student.classYear << endl;
+        file << "Dormitory B" << student.block << ", R-" << student.room << "\t\t" << "Section " << student.section << endl;
+        file << "---------------------------------------------" << endl;
+        file.close();
+    } else {
+        cerr << "Unable to open file" << endl;
+    }
+    
+    }
+
+void displayStudentInfo(const Student& student);
+
+void registerStudent(Student& student) {
+    cout << "Enter full name: ";
+    cin >> student.name;
+
+    cout << "Enter department: ";
+    cin >> student.department;
+
+    cout << "Enter ID number: ";
+    cin >> student.id;
+
+    cout << "Enter admission : ";
+    cin >> student.admission;
+
+    cout << "Enter admission year: ";
+    cin >> student.admissionYear;
+
+    cout << "Enter class year: ";
+    cin >> student.classYear;
+
+    cout << "Enter dormitory block : ";
+    cin >> student.block;
+
+    cout << "Enter room number: ";
+    cin >> student.room;
+
+    cout << "Enter section : ";
+    cin >> student.section;
+
+    // Clear the input buffer
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    saveStudentToFile(student);
+}
